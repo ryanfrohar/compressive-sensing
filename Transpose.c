0c6ee2fb3fb727456f40cfdbc6fece5c482ba795
architbhatia14 <architbhatia@cmail.carleton.ca>
2021-01-05 18:29:15 -0500

    Added some initalizations

--- Transpose.c
+++ Transpose.c
@@ -1,41 +1,56 @@
-//
-//  main.c
-//  project
-//
+
 //  Created by Archit Bhatia on 2020-12-25.
-//
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define M 3 // Amount of measurements taken
+#define N 5 // Amount of samples
+#define S 6 // Sparsity
+#define amount_Found 16 // amount of elements found
+#define iterations 11 // number of iterations
+int Matrix[M][N]; //Matrix composed of the measurements and samples
+
+
 
 /*
 Takes a matrix and prints the transpose of the matrix
 */
 
-void Transpose(int M, int N, int inputMatrix[M][N], int TransposeMatrix[N][M]){
+void Transpose(int inputMatrix[M][N], int TransposeMatrix[N][M]){
 
     int i, j;
-    for (i = 0; i < M; i++){
-        for(j = 0; j < N; j++){
+    for (i = 0; i < N; i++){
+        for(j = 0; j < M; j++){
             TransposeMatrix[i][j] = inputMatrix[j][i];
         }
     }
 }
 
 void main(){
-
-    int A[3][3] = {{1,2,3},
-                    {1,2,3},
-                    {1,2,3}};
-    int B[3][3], i, j;
-    Transpose(3,3,A,B);
+    
+    // populates the starting matrix with random values
+    for(int x = 0; x < M; x++){
+        for(int y = 0; y < N; y++){
+            Matrix[x][y] = rand()%100; // randomizes the values being filled with up to 100
+            printf("%d ", Matrix[x][y]); // prints the input matrix
+        }
+        printf("\n");
+    }
+    printf("\n");
     
     
-    for(i = 0; i < 3; i++){
-        for(j = 0; j < 3; j++){
+    int B[N][M], i, j;
+    Transpose(Matrix,B); //calls the transpose function
+    
+    //prints the transpose matrix
+    for(i = 0; i < N; i++){
+        for(j = 0; j < M; j++){
         	 printf("%d ", B[i][j]);
         }
         printf("\n");
     }
+     
     
 }
 
