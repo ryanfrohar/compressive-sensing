3bacd59d24f2af36fd6af01f6376fa284b5d2045
Ryan Frohar <ryan.frohar@gmail.com>
2021-01-07 13:29:38 -0500

    Modified Main

--- OMP.c
+++ OMP.c
@@ -3,16 +3,45 @@
 #include <stdlib.h>
 #include "math.h"
 
-#define M 3 // Amount of measurements taken
-#define N 5 // Amount of samples
+#define M 8 // Amount of measurements
+#define N 64 // Amount of samples
 #define S 6 // Sparsity
 #define amount_Found 16 // amount of elements found
 #define iterations 11 // number of iterations
-int Matrix[M][N]; //Matrix composed of the measurements and samples
 
-void calc_correlation(int N,int correlation[N],int norms[N],float inner_sum[N]){
+//ENV Variables
+int Rand_Mat[M][N]; //Matrix composed of the measurements and samples
+int NumberFound = 0;
+
+/**
+Function to return inner product of a specific column of a matrix and a row vector.
+
+matrix: 2x2 matrix containing float values. (float)
+vector: 2x1 vector containing float values. (float)
+matrixRows: Number of rows in the matrix being used. (int)
+matrixColumns: Number of columns in the matrix being used. Same as the vector size. (int)
+mulColumn: Index of the column being multiplied in the matrix. (int)
+
+**/
+
+float innerColMul(float vector[M], int mulColumn)
+{
+	float total = 0;  // Used to store the total product value of the multiplication
+	
+	// Loop to multiply all elements of the matrix's selected column by the vector's elements
+	for(int i=0; i<M ; i++) {
+		total = total + (Rand_Mat[i][mulColumn] * vector[i]);
+	}
+	
+	return total;
+}
+
+
+void calc_correlation(int correlation[N],int norms[N], int r[M]){
+	float temp;
 	for(int i = 0; i<N; i++){
-		correlation[i] = fabs(inner_sum[i]/norms[i]);
+		temp = innerColMul((float *) r, i);
+		correlation[i] = fabs(temp/norms[i]);
 	}
 }
 
@@ -21,7 +50,7 @@ void calc_correlation(int N,int correlation[N],int norms[N],float inner_sum[N]){
 number of rows of second matrix for it to work mathematically
 */
 
-void MatMultiplication(int M,int N,int n,int matrix1[M][N], int matrix2[N][n], int result[M][n]){
+void MatMultiplication(int n,int matrix1[M][N], int matrix2[N][n], int result[M][n]){
 	int i,j,k;
 	for(i=0;i<M;i++){
     		for(j=0;j<n;j++){
@@ -39,7 +68,7 @@ void MatMultiplication(int M,int N,int n,int matrix1[M][N], int matrix2[N][n], i
 	Subtracts two matrixes of same size into result
 
 */
-void MatSubtraction(int M,int N, int matrix1[M][N],int matrix2[M][N],int result[M][N]){
+void MatSubtraction(int matrix1[M][N],int matrix2[M][N],int result[M][N]){
 	//matricies must be same size to subtract
 	for(int i=0;i<M;i++){
     		for(int j=0;j<N;j++){
@@ -62,11 +91,11 @@ void printMatrix(int m,int n, int matrix[m][n]){
 		
 int max_index(float *array, int size){
 	int indexMax = 0;
-	float max = *(vector + indexMax);
+	float max = *(array + indexMax);
 	for (int i = 0; i < size; i++){
-		if (*(vector + i) > max)
+		if (*(array + i) > max)
 		{
-			max = *(vector + i);
+			max = *(array + i);
 			indexMax = i;
 		}
 	}
@@ -83,19 +112,6 @@ float norm_Col(float *array, int m, int n, int C){
 	return sum;
 }
 
-void union(int *vec, int newval) {
-	// Using this function values of index are sorted and there is no need to check for repitition
-	for (int i = 0;i<number_of_finded;i++){
-		if (*(vec + i) == newval){ 
-			//Ignore if repetition
-			return;
-		}
-	}
-	*(vec + number_of_finded) = newval; 
-	number_of_finded++;
-	bubbleSort(vec, number_of_finded);
-	return;
-}
 
 void swap(int *xp, int *yp){
 	int temp = *xp;
@@ -117,30 +133,22 @@ void bubbleSort(int *arr, int n){
 	return;
 }
 
-/**
 
-@Ross Matthew
-Function to return inner product of a specific column of a matrix and a row vector.
-
-matrix: 2x2 matrix containing float values. (float)
-vector: 2x1 vector containing float values. (float)
-matrixRows: Number of rows in the matrix being used. (int)
-matrixColumns: Number of columns in the matrix being used. Same as the vector size. (int)
-mulColumn: Index of the column being multiplied in the matrix. (int)
-
-**/
-
-float innerColMul(float matrix, float vector, int matrixRows, int matrixColumns, int mulColumn)
-{
-	float total = 0;  // Used to store the total product value of the multiplication
-	
-	// Loop to multiply all elements of the matrix's selected column by the vector's elements
-	for(int i=0; i<M ; i++) {
-		total = total + (matrix[i][mulColumn] * vector[i]);
+void unionMat(int *vec, int newval) {
+	// Using this function values of index are sorted and there is no need to check for repitition
+	for (int i = 0;i<NumberFound;i++){
+		if (*(vec + i) == newval){ 
+			//Ignore if repetition
+			return;
+		}
 	}
-	
-	return total;
+	*(vec + NumberFound) = newval; 
+	NumberFound++;
+	bubbleSort(vec, NumberFound);
+	return;
 }
+
+
 
 /**
 
@@ -151,13 +159,12 @@ Takes sparse signal and compares it to the array of max signal values and output
 **/
 
 
-float MSE(float sparse, float maxIndexes, int length)
+float MSE(float sparse[N], float maxIndexes[N], int length)
 {
 	float mse = 0;  // Initialize mean squared error value
-	
 	for( int i=0 ; i<length ; i++)
 	{
-		mse += (sparse[i] - maxIndexes[i]) ^ 2;  // Add the squared difference of each value in the array/vector
+		mse += (sparse[i] - maxIndexes[i])*(sparse[i] - maxIndexes[i]);  // Add the squared difference of each value in the array/vector
 	}
 	
 	return mse/length;  // return the average deviation based on the length of the entire array/vector being compared
@@ -171,7 +178,7 @@ The ratio is a result of the log of the sum of the MSE value of the sparse signa
 
 **/
 
-float SNR( float normSparse, float maxSparseIndexes, int length)
+float SNR( float normSparse[N], float maxSparseIndexes[N], int length)
 {
 	float signal = 0;  // Initialize signal, which is the sum of all the squared normalized sparse signal values
 	float snr = 0;  // Initialized value for signal to noise ratio
@@ -190,18 +197,19 @@ float SNR( float normSparse, float maxSparseIndexes, int length)
 Takes a matrix and prints the transpose of the matrix
 */
 
-void Transpose(int inputMatrix[M][N], int TransposeMatrix[N][M]){
+void transpose(int inputMatrix[M][N], int transposeMatrix[N][M]){
 
     int i, j;
     for (i = 0; i < N; i++){
         for(j = 0; j < M; j++){
-            TransposeMatrix[i][j] = inputMatrix[j][i];
+            transposeMatrix[i][j] = inputMatrix[j][i];
         }
     }
 }
 
 
 void testMarko(){
+	/**
 	int arr[2][3] = {{3,2,4}, {-4,5,1}};
 	int arr1[3][4] = {{3,0,-3,6},{7,5,-4,4},{-2,2,-1,1}};
 	int result[2][4] = {0};
@@ -222,14 +230,14 @@ int main(){
 	// populates the starting matrix with random values
 	for(int x = 0; x < M; x++){
 		for(int y = 0; y < N; y++){
-			Matrix[x][y] = rand()%100; // randomizes the values being filled with up to 100
-			printf("%d ", Matrix[x][y]); // prints the input matrix
+			Rand_Mat[x][y] = rand()%10; // randomizes the values being filled with up to 10
+			printf("%d ", Rand_Mat[x][y]); // prints the input matrix
 		}
 		printf("\n");
 	}
 	printf("\n");
 	int B[N][M], i, j;
-	Transpose(Matrix,B); //calls the transpose function
+	transpose(Rand_Mat, B); //calls the transpose function
     
 	//prints the transpose matrix
 	for(i = 0; i < N; i++){
